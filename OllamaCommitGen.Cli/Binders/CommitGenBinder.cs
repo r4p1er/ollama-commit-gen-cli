4948d41b82df4318e78ef4053f14cdd5a09614b3
Alexander Tsyganok <tsyganok2015@gmail.com>
2024-07-04 11:43:29 +0300

    Feat: add origin and model options
    Add new options to customize CommitGenService behavior. Origin specifies a custom remote Ollama host. Model specifies an Ollama model to be used

--- OllamaCommitGen.Cli/Binders/CommitGenBinder.cs
+++ OllamaCommitGen.Cli/Binders/CommitGenBinder.cs
@@ -1,3 +1,4 @@
+using System.CommandLine;
 using System.CommandLine.Binding;
 using OllamaCommitGen.Domain.Interfaces;
 using OllamaCommitGen.Domain.Services;
@@ -5,12 +6,17 @@ using OllamaCommitGen.Infrastructure.Services;
 
 namespace OllamaCommitGen.Cli.Binders;
 
-public class CommitGenBinder : BinderBase<ICommitGenService>
+public class CommitGenBinder(Option<string> originOption, Option<string> modelOption) : BinderBase<ICommitGenService>
 {
     protected override ICommitGenService GetBoundValue(BindingContext bindingContext)
     {
         var git = new GitService(Directory.GetCurrentDirectory());
-        var ollama = new OllamaService(new HttpClient(), "http://localhost:11434");
+        var ollama = new OllamaService(
+            new HttpClient(),
+            bindingContext.ParseResult.GetValueForOption(originOption)!
+        );
+
+        ollama.RequestBody.Model = bindingContext.ParseResult.GetValueForOption(modelOption)!;
 
         return new CommitGenService(git, ollama);
     }
--- OllamaCommitGen.Cli/Program.cs
+++ OllamaCommitGen.Cli/Program.cs
@@ -7,7 +7,25 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        var rootCommand = new RootCommand("Generate commit message via Ollama AI");
+        var originOption = new Option<string>(name: "--origin", description: "Specifies a custom remote Ollama host")
+        {
+            Arity = ArgumentArity.ExactlyOne,
+            IsRequired = false
+        };
+        originOption.AddAlias("-o");
+        originOption.SetDefaultValue("http://localhost:11434");
+
+        var modelOption = new Option<string>(name: "--model", description: "Specifies an Ollama model to be used")
+        {
+            Arity = ArgumentArity.ExactlyOne,
+            IsRequired = false
+        };
+        modelOption.AddAlias("-m");
+        modelOption.SetDefaultValue("llama3");
+        
+        var rootCommand = new RootCommand("Generates commit message via Ollama AI");
+        rootCommand.AddOption(originOption);
+        rootCommand.AddOption(modelOption);
         
         rootCommand.SetHandler(async (commitGenService) =>
         {
@@ -15,7 +33,7 @@ class Program
             Console.WriteLine("Generated message: " + message);
             commitGenService.Commit(message);
             commitGenService.Dispose();
-        }, new CommitGenBinder());
+        }, new CommitGenBinder(originOption, modelOption));
 
         await rootCommand.InvokeAsync(args);
     }
